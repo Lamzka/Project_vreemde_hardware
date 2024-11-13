e99f7435a53032601dd93d706f371407f7bcd1d4
Alice <118888832+Roman220287@users.noreply.github.com>
2024-11-13 10:41:10 +0100

    Star/fuel spawning finished
    now spawns randomly in a designated area based on where the where the player is in the area

--- Assets/Development/Roman/Scripts/Star spawning.cs
+++ Assets/Development/Roman/Scripts/Star spawning.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Starspawning : MonoBehaviour
 {
     [SerializeField] private GameObject starPrefab;
+    [SerializeField] private Transform designatedSpawnArea;
     [SerializeField] private float spawnTimer;
 
-    void Update()
+    private void StarSpawning()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+        
+        Instantiate<GameObject>(starPrefab, randomPosition, Quaternion.identity, designatedSpawnArea);
+
+        spawnTimer = 3;
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         spawnTimer -= Time.deltaTime;
 
@@ -15,15 +25,6 @@ public class Starspawning : MonoBehaviour
         {
             StarSpawning();
         }
-    }
-
-    private void StarSpawning()
-    {
-        Vector3 randomPosition = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));
-        
-        Instantiate<GameObject>(starPrefab, randomPosition, Quaternion.identity);
-
-        spawnTimer = 1;
     }
 
 }
