0d86fbb55ad506e22a57e5085fec9cdb35062328
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2024-11-14 14:45:27 +0100

    Quest list beginning
    all packages have a name and adress. recievers name/planet are visible on the quest list

--- Assets/Development/Esmee/Scripts/Package.cs
+++ Assets/Development/Esmee/Scripts/Package.cs
@@ -1,25 +1,62 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Package : MonoBehaviour
 {
     public PackageInfo Info;
+    public List<GameObject> PackageList = new List<GameObject>();
+    [SerializeField] private GameObject packagePrefab;
+
+    [SerializeField] private TMP_Text questList;
+
     void Start()
     {
-        if (Info != null && Info.RecieverPlanet.Length > 0)
-        {
-            string randomPlanet = Info.RecieverPlanet[Random.Range(0, Info.RecieverPlanet.Length)];
-            Debug.Log("Random Planet: " + randomPlanet);
-        }
-
-        if (Info != null && Info.RecieverName.Length > 0)
-        {
-            string randomName = Info.RecieverName[Random.Range(0, Info.RecieverName.Length)];
-            Debug.Log("Random Name: " + randomName);
-        }
+        AllPackages();
+        Names();
     }
 
     void Update()
     {
     }
 
+
+    private void AllPackages()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            PackageList.Add(packagePrefab);
+        }
+        //SpawnPackages(); //of in start?
+    }
+
+    /* private void SpawnPackages()
+     {
+         for (int i = 0; i < PackageList.Count; ++i)
+         {
+            //we kunnen zeggen van ze spawnen in, of ze staan al in de scene en dan sleep je ze in de allpackages list handmatig
+         }
+     }*/
+
+    private void Names()
+    {
+        if (Info == null || Info.RecieverPlanet.Length == 0 || Info.RecieverName.Length == 0)
+        {
+            return;
+        }
+
+        System.Text.StringBuilder nameListBuilder = new System.Text.StringBuilder();
+
+        for (int i = 0; i < PackageList.Count; ++i)
+        {
+            string randomPlanet = Info.RecieverPlanet[Random.Range(0, Info.RecieverPlanet.Length)];
+            string randomName = Info.RecieverName[Random.Range(0, Info.RecieverName.Length)];
+
+            nameListBuilder.AppendLine($"{randomName} - {randomPlanet}");
+        }
+
+        questList.text = nameListBuilder.ToString();
+    }
 }
+
+
