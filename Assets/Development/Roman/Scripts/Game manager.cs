98fcaccd0a9bd28975f1302389a90b7446290a91
Alice <118888832+Roman220287@users.noreply.github.com>
2024-11-15 11:42:26 +0100

    Gamemanager progression
    Not finalized but works kinda

--- Assets/Development/Roman/Scripts/Game manager.cs
+++ Assets/Development/Roman/Scripts/Game manager.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform[] planets;
+    [SerializeField] private GameObject destinationMarker;
+    [SerializeField] private GameObject package;
+    [SerializeField] private bool pickedUpPackage;
+    [SerializeField] private bool ifDroped;
 
     void Update()
     {
-        
+        if (pickedUpPackage)
+        {
+            SetMarker();
+        }
+        if (ifDroped)
+        {
+            DropofCheck();
+        }
+    }
+
+    private void SetMarker()
+    {
+        destinationMarker.transform.position = planets[Random.Range(0, planets.Length)].position;
+    }
+
+    private void DropofCheck()
+    {
+        if (package.transform.position != destinationMarker.transform.position)
+        {
+            Debug.Log("Wrong planet");
+        }
+        else
+        {
+            Debug.Log("Right planet");
+        }
     }
 }
