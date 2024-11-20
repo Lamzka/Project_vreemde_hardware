bf8a2b68ba9262fa6fe04e2e57c139fc4cc4bfe7
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2024-11-20 13:05:49 +0100

    fuel displayed in %

--- Assets/Development/Roman/Scripts/Fuel depletion.cs
+++ Assets/Development/Roman/Scripts/Fuel depletion.cs
@@ -1,20 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class Fueldepletion : MonoBehaviour
 {
     [SerializeField] private TMP_Text fuelText;
+    [SerializeField] private GameObject sosCanvas;
     public float Fuel;
 
     void Update()
+    {
+        fuelDropping();
+    }
+
+    private void fuelDropping()
     {
         if (Fuel >= 0)
         {
             Fuel -= Time.deltaTime;
         }
 
-        fuelText.text = Fuel.ToString();
+        fuelText.text = Fuel.ToString("F0") + "%";
+        noFuelLeft();
+    }
+
+    private void noFuelLeft()
+    {
+        if (Fuel <= 0.01)
+        {
+            Fuel = 0;
+            sosCanvas.SetActive(true);
+            //returning to base in 3..2..1..
+            //tp naar bigshipinc
+            //verlies geld door package damage?
+            //ga verder
+        }
     }
 }
