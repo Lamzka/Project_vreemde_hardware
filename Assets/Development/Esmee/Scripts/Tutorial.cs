fb2dd7282a8d2dc0880d3e00b194588763244c17
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2024-11-25 12:21:20 +0100

    small tutorial update

--- Assets/Development/Esmee/Scripts/Tutorial.cs
+++ Assets/Development/Esmee/Scripts/Tutorial.cs
@@ -1,14 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
-public class Tutorial : MonoBehaviour
+public class Tutorial : MonoBehaviour, IButtonInput
 {
     void Start()
     {
 
     }
+    void OnEnable()
+    {
+        GameObject.FindGameObjectWithTag("InputManagers").GetComponent<ButtonInputSubject>().SetListeners(this); //input
+    }
+
+    void OnDisable()
+    {
+        GameObject.FindGameObjectWithTag("InputManagers").GetComponent<ButtonInputSubject>().RemoveListeners(this); //input
+    }
 
     void Update()
     {
 
+    }
+
+    IEnumerator WaitSomeTime()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+
+
+    public void OnButton(int button, bool state) //input
+    {
+        Next(button);
+    }
+
+    private void Next(int button)
+    {
+        if (button == 0)
+        {
+
+        }
     }
 }
