4c26a2eed22961e13f3bfeb051f2a16ffd9ef503
Lamzka <Raymond0besson@Gmail.com>
2024-11-27 15:15:00 +0100

    Ship Rotation WIP

--- Assets/Development/Roman/Scripts/Sideways ship rotation.cs
+++ Assets/Development/Roman/Scripts/Sideways ship rotation.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class Sidewaysshiprotation : MonoBehaviour, IButtonInput
 {
-    [SerializeField] private float minTurnSpeed;
-    [SerializeField] private float maxTurnSpeed;
+    public float Torque = 0.5f;
+
+    public float SmoothingTime;
 
     private Rigidbody rigidbody;
 
-    public float Torque = 0.5f;
+    [SerializeField] private float maxDrag;
+    [SerializeField] private float minTurnSpeed;
+    [SerializeField] private float maxTurnSpeed;
+
+    private Vector3 rotationDirection;
+
+    private int currenttButton;
+
+    private bool isBeingPressed;
+
+
+
 
     private void Start()
     {
@@ -26,29 +38,61 @@ public class Sidewaysshiprotation : MonoBehaviour, IButtonInput
 
     void FixedUpdate()
     {
-        rigidbody.angularVelocity = Vector3.forward * Mathf.Clamp(rigidbody.angularVelocity.z, minTurnSpeed, maxTurnSpeed);
+
+
+        if (isBeingPressed)
+        {
+            rigidbody.angularVelocity = Vector3.forward * Mathf.Clamp(rotationDirection.z, minTurnSpeed, maxTurnSpeed);
+            RotateRight();
+        }
+        else
+        {
+            rigidbody.angularVelocity = Vector3.Lerp(rigidbody.angularVelocity, Vector3.zero, SmoothingTime * Time.deltaTime);
+        }
+
+
+
+
     }
 
-    public void OnButton(int Button, bool state)
+    public void OnButton(int button, bool state)
     {
-        switch (Button, state)
+        HandleInput(button, state);
+    }
+
+    private void HandleInput(int button, bool state)
+    {
+        switch (button, state)
         {
+            default:
+                isBeingPressed = false;
+                /*rotationDirection = Vector3.zero;*/
+                break;
             case (4, true):
-                RotateRight();
+                rotationDirection = Vector3.back;
+                isBeingPressed = true;
                 break;
             case (5, true):
-                RotateLeft();
+                rotationDirection = Vector3.forward;
+                isBeingPressed = true;
+                break;`
+            case (12, true):
+                rotationDirection = Vector3.right;
+                isBeingPressed = true;
+                break;
+            case (13, true):
+                rotationDirection = Vector3.left;
+                isBeingPressed = true;
                 break;
         }
     }
 
     private void RotateRight()
     {
-        rigidbody.AddRelativeTorque(Vector3.forward * Torque, ForceMode.Impulse);
+        /*rigidbody.angularDrag = Mathf.Lerp(maxDrag, 0.5f, SmoothingTime * Time.deltaTime);*/
+        rigidbody.AddRelativeTorque(rotationDirection * Torque, ForceMode.Force);
+
     }
 
-    private void RotateLeft()
-    {
-        rigidbody.AddRelativeTorque(Vector3.back * Torque, ForceMode.Impulse);
-    }
+
 }
