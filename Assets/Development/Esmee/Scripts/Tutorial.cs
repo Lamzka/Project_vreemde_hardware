54d284df4dc4277746c344b697d49c424ef13b29
Lamzka <Raymond0besson@gmail.com>
2024-12-12 12:19:40 +0100

    Tutorial Rewrite, Testing needed

--- Assets/Development/Esmee/Scripts/Tutorial.cs
+++ Assets/Development/Esmee/Scripts/Tutorial.cs
@@ -4,21 +4,34 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
+public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput, IPedalInput
 {
+    //Serialized Fields
     [SerializeField] private List<GameObject> sprites = new List<GameObject>(); //list with all slides
+
     [SerializeField] private List<AudioClip> aiVoice = new List<AudioClip>(); //list with all audio clips
+
     [SerializeField] private AudioSource audioSrc; //audio source
-    private int currentSlide = 0; //slides start at the first one
 
-    private int RequestedButton;
-    private int currentButtonPressed;
+    //Public Fields
+    public float RequestedMaxLeftWheelRotation;   //Rotation needed to proceed
+    public float RequestedMaxRightWheelRotation;  //Rotation needed to proceed
 
-    private bool TaskOngoing = true;
+    public float RequestedGasPedalInput = 50;    //Gas pedal input needed to proceed
+    public float RequestedBreakPedalInput = 50;  //Break pedal input needed to proceed
 
-    private float currentWheelRotation;
+    //Private Fields
+    private int currentSlide = 0;     //Slides start at the first one
+    private int RequestedButton;      //Button that the player needs to press to proceed
+    private int currentButtonPressed; //Button the player is currently pressing
+ 
+    private float currentWheelRotation;   //Current Rotation of the steering wheel
+    private float currentGasPedalInput;   //Current Input of the gaspedal
+    private float currentBreakPedalInput; //Current Input of the beakpedal
 
-    private bool isNotPlaying()
+    private bool TaskOngoing = true;   //Is true when Player input is needed
+
+    private bool isNotPlaying()   //Check if the audioSorce is playing a sound
     {
         if (audioSrc.isPlaying)
         {
@@ -30,7 +43,7 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         }   
     }
 
-    private bool buttonRequitementCheck()
+    private bool buttonRequitementCheck()   //Checks if player button input is equal to whats requested
     {
         if (currentButtonPressed == RequestedButton)
         {
@@ -42,9 +55,9 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         }
     }
 
-    private bool isWheelToTheLeft()
+    private bool isWheelToTheLeft()   //Checks if the wheel is over the requested value
     {
-       if(currentWheelRotation > 0)
+       if(currentWheelRotation > RequestedMaxLeftWheelRotation)
         {
             return true;
         }
@@ -54,9 +67,9 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         }
     }
 
-    private bool isWheelToTheRight()
+    private bool isWheelToTheRight()   //Checks if the wheel is over the requested value
     {
-        if (currentWheelRotation > 0)
+        if (currentWheelRotation > RequestedMaxRightWheelRotation)
         {
             return true;
         }
@@ -66,11 +79,28 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         }
     }
 
-    private void Start()
+    private bool isGasPedalPressed()   //Checks if the gas pedal is pressed
     {
-        audioSrc.Stop();
-        currentSlide = 0;
-        TaskOngoing = false;    
+        if(currentGasPedalInput > RequestedGasPedalInput)
+        {
+            return true;
+        }
+        else
+        {
+            return false ;
+        }
+    }
+
+    private bool isBreakPedalPressed()   //Checks if the break pedal is pressed
+    {
+        if (currentBreakPedalInput > RequestedBreakPedalInput)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void OnButton(int button, bool State)
@@ -83,49 +113,82 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         input = currentWheelRotation;
     }
 
+    public void OnGasPedal(float GasValue)
+    {
+        GasValue = RequestedGasPedalInput;
+    }
 
+    public void OnBreakPedal(float BreakValue)
+    {
+        BreakValue = RequestedBreakPedalInput;
+    }
+
+    private void Start() 
+    {
+        audioSrc.Stop();
+        currentSlide = 0;
+        TaskOngoing = false;    
+    }
 
     private void Update()
     {
-        if (!TaskOngoing && isNotPlaying())
+        if (!TaskOngoing && isNotPlaying())   //if a task is not ongoing and the audioSource is not playing any sound
         {
 
-            switch (currentSlide)
+            switch (currentSlide)   //Switch case to call the functions needed for the current slide 
             {
-                case (0): TutorialStart(); break;
-                case (1): Ascend(); break;
+                case (0): WaitUntilAudioIsFinished(false); break;       //intro
+                case (1): WaitForButtonInput(0);  break;                //startup/acend
+                case (2): WaitUntilAudioIsFinished(false); break;       //wheel intro
+                case (3): WaitForWheelTurn(false); break;               //turn left
+                case (4): WaitForWheelTurn(true); break;                //turn right
+                case (5): WaitForButtonInput(4); break;                 // wheel triggers
+                case (6): WaitTillAudioIsFinished(false); break;        //shifter info
+                case (7): WaitForButtonInput(14); break;                //Shifter up
+                case (8): WaitForButtonInput(15); break;                //Shifter down
+                case (9): WaitUntilAudioIsFinished(false); break;       //Pedal intro
+                case (10): WaitForPedals(true); break;                  //Gas pedal
+                case (11): WaitForPedals(false); break;                 //Break pedal
+                case (12): WaitTillAudioIsFinished(false); break;       //Package grab intro
+                case (13): WaitForButtonInput(1); break;                //grab Package
+                case (14): WaitForButtonInput(1); break;                //Drop Package
+                case (15): WaitUntilAudioIsFinished(true); break;       //Tutorial End
             }
         }
 
       
     }
 
-    private void playClip()
+    private void playClip()   //Sets and plays an audioclip acording to the slide count
     {
         audioSrc.Stop();
         audioSrc.clip = aiVoice[currentSlide];
         audioSrc.Play();
     }
-    private void NextSlide()
+
+    private void NextSlide()   //Progresses slides
     {
         currentSlide++;
         TaskOngoing = false;
     }
 
 
-    private void TutorialStart()
+    private void WaitUntilAudioIsFinished(bool IsTutorialEnd)   //Waits until the audioSource isnt playing any sound and proceeds to next slide
     {
         TaskOngoing = true;
 
         if (isNotPlaying())
         playClip();
 
-        StartCoroutine(WaitTillAudioIsFinished());
+        StartCoroutine(WaitTillAudioIsFinished(IsTutorialEnd));
+
     }
 
-    private void Ascend()
+    private void WaitForButtonInput(int buttonIndex)   //Wait until the player has pressed requested button
     {
         TaskOngoing = true;
+
+        RequestedButton = buttonIndex;
 
         if (isNotPlaying())
             playClip();
@@ -133,17 +196,43 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
         StartCoroutine(WaitTillButtonPress());
     }
 
-    private void TurnToLeft()
+    private void WaitForWheelTurn(bool DirectionSwitch)   //waits untill player has turned the wheel to the left
     {
+        TaskOngoing = true;
 
+        if (isNotPlaying())
+            playClip();
+
+        StartCoroutine(WaitTillWheelHasTurned(DirectionSwitch));
     }
 
    
-    IEnumerator WaitTillAudioIsFinished()
+    private void WaitForPedals(bool PedalSwitch)   //waits until the player has pressed requested pedal
+    {
+        TaskOngoing = true;
+        if (isNotPlaying())
+            playClip();
+
+        StartCoroutine(WaitTillPedalsArePressed(PedalSwitch));
+    }
+
+
+
+    //true = Change to main Scene
+    //false = Change to next slide
+    IEnumerator WaitTillAudioIsFinished(bool IsTutorialEnd)   
     {
         yield return new WaitUntil(isNotPlaying);
-        NextSlide();
-        TaskOngoing = false;
+
+        if (IsTutorialEnd)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        else if (!IsTutorialEnd)
+        {
+            NextSlide();
+            TaskOngoing = false;
+        }   
     }
 
     IEnumerator WaitTillButtonPress()
@@ -154,157 +243,40 @@ public class Tutorial : MonoBehaviour, IButtonInput, IWheelInput
 
     }
 
-    IEnumerator WaitTillWheelHasTurnedLeft()
+
+    //true = Right
+    //false = Left
+    IEnumerator WaitTillWheelHasTurned(bool DirectionSwitch)
     {
-        yield return new WaitUntil(isWheelToTheLeft);
+
+        if (DirectionSwitch)
+        {
+            yield return new WaitUntil(isWheelToTheRight);
+        }
+        else if (!DirectionSwitch)
+        {  
+            yield return new WaitUntil(isWheelToTheLeft);
+        }
+        
         NextSlide();
         TaskOngoing = false;
     }
 
-    IEnumerator WaitTillWheelHasTurnedRight()
+    //true = GasPedal
+    //false = BreakPedal
+    IEnumerator WaitTillPedalsArePressed(bool PedalSwitch)
     {
-        yield return new WaitUntil(isWheelToTheLeft);
+        if (PedalSwitch)
+        {
+            yield return new WaitUntil(isGasPedalPressed);
+        }
+        else if (!PedalSwitch)
+        {
+            yield return new WaitUntil(isBreakPedalPressed);
+        }
+
         NextSlide();
         TaskOngoing = false;
     }
-
-
-
-
-
-
-
-
-
-    /*  IEnumerator NextSlide()
-      {
-          for (int i = 0; i < sprites.Count; i++) // Loop through all slides
-          {
-              if (currentSlide >= 0)
-              {
-                  *//* sprites[currentSlide].SetActive(false); // turn off the last slide*//*
-              }
-
-              *//* currentSlide = i;
-               sprites[currentSlide].SetActive(true);*//* //activate the next slide
-
-              if (i < aiVoice.Count)
-              {
-                  audioSrc.clip = aiVoice[i];
-                  audioSrc.Play(); //start playing audio
-
-                  while (audioSrc.isPlaying) //waits until the audio is finished playing
-                  {
-                      yield return null;
-                  }
-              }
-
-              yield return new WaitForSeconds(3f); //wait a few seconds before the next slide
-          }
-
-          SceneManager.LoadScene("MainScene"); //load main scene
-      }
-
-  */
-
-
-    /* public bool buttonpressed;
-     private bool audioIsFinished;
-
-     void OnEnable()
-     {
-         GameObject.FindGameObjectWithTag("InputManagers").GetComponent<ButtonInputSubject>().SetListeners(this); //Set this class as a listener to the ButtonInputSubject
-     }
-
-     void OnDisable()
-     {
-         GameObject.FindGameObjectWithTag("InputManagers").GetComponent<ButtonInputSubject>().RemoveListeners(this); //remove this class as a listener to the ButtonInputSubject
-     }
-
-     private void Start()
-     {
-         WelcomePilot();
-     }
-
-     public void OnButton(int button, bool State)
-     {
-
-         PressX(button);
-     }
-
-
-
-     private void WelcomePilot()
-     {
-         sprites[0].SetActive(true);
-         audioSrc.clip = aiVoice[0];
-         audioSrc.Play();
-         StartCoroutine(WaitForAudioToFinish(() => PressX(0)));
-     }
-
-     private void PressX(int button)
-     {
-         sprites[1].SetActive(true);
-         audioSrc.clip = aiVoice[1];
-         audioSrc.Play();
-         if (button == 0 || buttonpressed == true)
-         {
-             StartCoroutine(WaitForAudioToFinish(() => UseShifter(15)));
-         }
-     }
-
-     private void UseShifter(int button)
-     {
-         sprites[2].SetActive(true);
-         audioSrc.clip = aiVoice[2];
-         audioSrc.Play();
-         if (button == 14 || button == 15)
-         {
-             StartCoroutine(WaitForAudioToFinish(() => SteerWheel()));
-         }
-     }
-
-     private void SteerWheel()
-     {
-         sprites[3].SetActive(true);
-         audioSrc.clip = aiVoice[3];
-         audioSrc.Play();
-         StartCoroutine(WaitForAudioToFinish(() => GiveGas()));
-     }
-
-     private void GiveGas()
-     {
-         sprites[4].SetActive(true);
-         audioSrc.clip = aiVoice[4];
-         audioSrc.Play();
-         StartCoroutine(WaitForAudioToFinish(() => GrabPackage(1)));
-     }
-
-     private void GrabPackage(int button)
-     {
-         sprites[5].SetActive(true);
-         audioSrc.clip = aiVoice[5];
-         audioSrc.Play();
-         if (button == 1)
-         {
-             StartCoroutine(WaitForAudioToFinish(() => ReadyPilot()));
-         }
-     }
-
-     private void ReadyPilot()
-     {
-         sprites[6].SetActive(true);
-         audioSrc.clip = aiVoice[6];
-         audioSrc.Play();
-         //wacht paar secs
-         SceneManager.LoadScene("MainScene");
-
-     }
-
-     private IEnumerator WaitForAudioToFinish(System.Action callback)
-     {
-         yield return new WaitUntil(() => !audioSrc.isPlaying);
-         callback?.Invoke();
-     }*/
 
 }
